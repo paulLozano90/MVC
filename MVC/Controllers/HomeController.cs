997fb2e950e88c3a00dc7a2ea79bb4923facb7d7
paulLozano90 <plc90@hotmail.es>
2015-11-10 14:23:50 +0100

    MVC Ejemplo

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Saludo> lista = new List<Saludo>()
+        {
+            new Saludo() {Idioma = "Es", Texto = "Buenos dias"},
+            new Saludo() {Idioma = "En", Texto = "Good morning"},
+            new Saludo() {Idioma = "Fr", Texto = "Bon jour"},
+            new Saludo() {Idioma = "De", Texto = "Guten morgen"}
+        };
+
+        // GET: Home
+        public ActionResult Index(String id)
+        {
+            var data = lista.FirstOrDefault(o => o.Idioma == id);
+            if (data == null)
+                data = lista.First(o => o.Idioma == "Es");
+            return View(data);
+        }
+
+
+    }
+}
---
+++ MVC/Models/Saludo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Saludo
+    {
+        public String Idioma { get; set; }
+        public String Texto { get; set; }
+
+
+    }
+}
