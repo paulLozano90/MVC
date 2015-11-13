c863b3a32136d9e6d70864fdba837e2c3ac1963d
paulLozano90 <plc90@hotmail.es>
2015-11-13 14:28:09 +0100

    PracticaAlmacen

---
+++ MvcPracticaAlmacen/Controllers/AlmacenController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcPracticaAlmacen.Models;
+
+namespace MvcPracticaAlmacen.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        private Tienda19Entities db = new Tienda19Entities();
+
+        // GET: Almacen
+
+        public ActionResult IndexAlmacen()
+        {
+            var data = db.Almacen;
+            return View(data);
+        }
+
+        public ActionResult DetalleAlmacen(int id)
+        {
+            var data = db.Almacen.Find(id);
+            return View(data);
+        }
+
+        //Modificar:
+        public ActionResult ModificarAlmacen(int id)
+        {
+            var data = db.Almacen.Find(id);
+            return View(data);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModificarAlmacen(Almacen model)
+        {
+            //Estado de como se encuentra el objeto
+            if (ModelState.IsValid)
+            {
+                //Recuperamos el objeto, cambiamos los datos y guardamos
+                db.Entry(model).State=EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("IndexAlmacen");
+            }
+            return View(model);
+        }
+
+        //Se puede borrar de dos maneras:
+
+        //Borrarlo directamente
+        public ActionResult BorrarAlmacen(int id)
+        {
+            var data = db.Almacen.Find(id);
+            //Si no esta vacio, tenemos que borrar antes los datos de ese almacen
+            if (data.ProductoAlmacen.Any())
+            {
+                db.ProductoAlmacen.RemoveRange(data.ProductoAlmacen);
+            }
+            db.Almacen.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("IndexAlmacen");
+        }
+
+        //Preguntar antes de borrar
+        //[HttpPost]
+        //public ActionResult BorrarAlmacen(Almacen model)
+        //{
+        //    if (model == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    var id = db.Almacen.Find(model.idAlmacen);
+        //    db.Almacen.Remove(id);
+        //    db.SaveChanges();
+        //    return View(model);
+        //}
+    }
+}
---
+++ MvcPracticaAlmacen/Controllers/ProductoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcPracticaAlmacen.Models;
+
+namespace MvcPracticaAlmacen.Controllers
+{
+    public class ProductoController : Controller
+    {
+        Tienda19Entities db=new Tienda19Entities();
+
+        // GET: Producto
+
+        public ActionResult Index()
+        {
+            var data = db.Producto;
+            return View(data);
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            var data = db.Producto.Find(id);
+            return View(data);
+        }
+        
+    }
+}
