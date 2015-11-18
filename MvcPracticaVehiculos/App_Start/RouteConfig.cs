540f3f39d238616d0b4028805cca37d335696c20
paulLozano90 <plc90@hotmail.es>
2015-11-18 14:24:26 +0100

    Pratica: Listado de tipos, modificacion y borrado.

---
+++ MvcPracticaVehiculos/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcPracticaVehiculos
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(
+                name: "Modificar",
+                url: "item/{nombreTipo}",
+                defaults: new
+                {
+                    controller = "Tipo",
+                    action = "ModificarTipo",
+                    nombre = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Tipo",
+                                action = "IndexTipo",
+                                id = UrlParameter.Optional }
+            );
+        }
+    }
+}
