d3fad233264a95f2c1f960f022982f06e7a7e537
paulLozano90 <plc90@hotmail.es>
2015-11-23 13:08:34 +0100

    Controlador

---
+++ AutenticacionPractica/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutenticacionPractica.Utilidades;
+
+namespace AutenticacionPractica.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var cifrado = SeguridadUtilidades.Cifrar("Hola Amigo", "AAA");
+            var descrifrado = SeguridadUtilidades.DesCifrar(cifrado, "AAA");
+            return View();
+        }
+    }
+}
